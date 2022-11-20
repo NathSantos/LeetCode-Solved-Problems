a96484c1f1b3f1df750b84995a00495d8e7ffd6b
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2022-11-20 10:52:32 -0400

    Not Finished Yet

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -15,7 +15,50 @@ Output: [7,0,8]
 Explanation: 342 + 465 = 807.
 """
 
+class Node:
+    def __init__(self, atual):
+        self.atual = atual
+        self.proximo = None
+        return
+
+    def add_prox(self, prox):
+        self.proximo = prox
+        return
+
+class LinkedList:
+    def __init__(self):       
+        self.head = None
+        self.tail = None
+        return
+
+    def add_node(self, no):
+        novo_no = Node(no)
+        
+        if self.head == None:
+            self.head = novo_no
+            novo_no.add_prox(None)
+        else:
+            self.tail.proximo = novo_no
+        self.tail = novo_no
+        
+
 def addTwoNumbers(l1, l2):
+    # criando a lista encadeada
+    linked1 = LinkedList.__init__
+    linked2 = LinkedList.__init__
+    print(linked1)
+    print(linked2)
+
+    for i in range(len(l1)-1,-1,-1):
+        linked1.add_node(l1[i])
+    for i in range(len(l2)-1,-1,-1):
+        linked2.add_node(l2[i])
+    print(linked1)
+    print(linked2)
+    return
+
+
+'''def addTwoNumbers(l1, l2):
     soma1 = ""
     soma2 = ""
     for i in range(len(l1)-1,-1,-1):
@@ -24,14 +67,12 @@ def addTwoNumbers(l1, l2):
         soma2 += str(l2[i])
 
     somaTotal = int(soma1)+int(soma2)
-    print(somaTotal)
     output = []
 
     while(somaTotal):
-        print(somaTotal%10)
         output.append(somaTotal%10)
         somaTotal = somaTotal//10
 
-    return output
+    return output'''
 
-print(addTwoNumbers([0],[0]))
+print(addTwoNumbers([2,4,3],[5,6,4]))
